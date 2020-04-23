753a626edeba6ad73085d1f42769d15c337b1cfe
urkeev14 <bozidar.mastilovicc@gmail.com>
2020-04-23 15:24:31 +0200

    quick fix

--- src/main/java/mastilovic/bozidar/fon/fisweb/frontcontroller/FrontController.java
+++ src/main/java/mastilovic/bozidar/fon/fisweb/frontcontroller/FrontController.java
@@ -11,7 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mastilovic.bozidar.fon.fisweb.action.viewresolver.ViewResolver;
+import mastilovic.bozidar.fon.fisweb.viewresolver.ViewResolver;
 import mastilovic.bozidar.fon.fisweb.controller.ApplicationController;
 
 /**
